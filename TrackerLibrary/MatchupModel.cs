a5981f8730d006020d6d2f4032d49fa89980a7b0
Jovy Ong <jovy83@live.com>
2019-09-18 21:29:57 +0800

    Add project files.

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary {
+    public class MatchupModel {
+        public List<MatchupEntryModel> matchupEntries { get; set; } = new List<MatchupEntryModel>();
+        public TeamModel TeamModel { get; set; }
+        public int MatchupRound { get; set; }
+    }
+}
---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary {
+    public class TeamModel {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>(); // initialize to an empty list of Person by default
+        public string TeamName { get; set; }
+    }
+}
